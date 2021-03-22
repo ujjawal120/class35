cd3c73c6d002e3dd7c4df25b31eae2b7713ce244
ujjawal120 <75742630+ujjawal120@users.noreply.github.com>
2021-03-22 21:07:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+var  hypnoticBall;
+var database;
+var position;
+function setup(){
+    database=firebase.database();
+    createCanvas(500,500);
+    hypnoticBall = createSprite(250,250,10,10);
+    hypnoticBall.shapeColor = "red";
+
+    var hypnoticBallPosition=database.ref('ball/position')
+    hypnoticBallPosition.on("value",readPosition);
+
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+    database.ref('ball/position').set({
+        'x':position.x+x,
+        'y':position.y+y
+    })
+}
+function readPosition(pos){
+    position=pos.val();
+    hypnoticBall.x=position.x;
+    hypnoticBall.y=position.y;
+}
